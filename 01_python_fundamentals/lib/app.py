02d06198ef86ee79298e3ef5a83d532602296fba
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-22 15:01:45 -0400

    afternoon lecture

--- 01_python_fundamentals/lib/app.py
+++ 01_python_fundamentals/lib/app.py
@@ -6,7 +6,7 @@
 	# Python datatypes 
 
 # 🚨 To enable ipdb debugging, first import "ipdb"
-import ipdb
+# import ipdb
 #set_trace which is a debugger you turn on when you want to check for bugs
 
 # 1. ✅ Create a condition to check a pet's mood
@@ -16,32 +16,58 @@ import ipdb
 
     # Note => Feel free to set your own values for "pet_mood" to view various outputs.
 
-pet_mood = "Hungry!"
-pet_name = "Rose"
-print(pet_mood)
+# pet_mood = "Hungry!"
+# pet_name = "Rose"
+# print(pet_mood)
 
 #conditional syntax
 
-if pet_mood == "Hungry!":
-    print("Rose needs to be fed.")
-elif pet_mood == "Rowdy":
-    print("Rose needs a walk.")
-else:
-    print("Rose is all good.")
+# if pet_mood == "Hungry!":
+#     print("Rose needs to be fed.")
+# elif pet_mood == "Rowdy":
+#     print("Rose needs a walk.")
+# else:
+#     print("Rose is all good.")
 
-# 2. ✅ Create a ternary operator using "pet_mood" as a condition:
-    # If pet_food is "Hungry!" => "Rose needs to be fed."
-    # In all other cases => "Rose is all good."
-print("Rose needs to be fed.") if pet_mood == "Hungry!" else print("Rose is all good.")
+# # 2. ✅ Create a ternary operator using "pet_mood" as a condition:
+#     # If pet_food is "Hungry!" => "Rose needs to be fed."
+#     # In all other cases => "Rose is all good."
+# print("Rose needs to be fed.") if pet_mood == "Hungry!" else print("Rose is all good.")
 
 # 3. ✅ Create a function (say_hello) that returns the string "Hello, world!"
     # Test invocation of "say_hello" in ipdb using "say_hello()"
     # say_hello() => "Hello, world!"
 
+#python => def(define)
+#Javascript => function
+
+# import ipdb
+
+# def say_hello():
+#     print("hello, world!")
+#     return "Hello, world!"
+
+# say_hello()
+# ipdb.set_trace()
+
+# #default argument
+# def say_hello( param = "hello" ):
+#     print(param)
+#     return "Hello World!"
+
+# say_hello()
+
 # 4. ✅ Create a function (pet_greeting) that will return a string with interpolated pet's name
     # Test invocation of "pet_greeting" in ipdb using "pet_greeting()"
     # pet_greeting("Rose") => "Rose says hello!"
     # pet_greeting("Spot") => "Spot says hello!"
+
+# def pet_greeting(name):
+#     return f"{name} says hello!"
+
+# print(pet_greeting("Rose"))
+# print(pet_greeting("Spot"))
+    
 
 # 5. ✅ Move conditional logic from Deliverable 1 into a function (pet_status) so that we may use it with different pets / moods
     # Test invocation of "pet_status" in ipdb using "pet_status(pet_name, pet_mood)"
@@ -51,11 +77,41 @@ print("Rose needs to be fed.") if pet_mood == "Hungry!" else print("Rose is all 
     
     # Take a moment to note that "pet_name" and "pet_mood" parameters are within Local Scope and take priority over "pet_name" and "pet_mood"
     # in Global Scope.
+# import ipdb
+# def pet_status(name, mood):
+#     if mood == "Hungry!":
+#         ipdb.set_trace()
+#         return f"{name} needs to be fed."
+#     elif mood == "Rowdy!":
+#         return f"{name} needs a walk."
+#     else:
+#         return f"{name} is all good."
+    
+# print(pet_status("Rose", "Hungry!"))
+# print(pet_status("Spot", "Rowdy!"))
+# print(pet_status("Bud", "Relaxed"))
+
+    
 
 # 6. ✅ Create a function (pet_birthday) that will increment a pet's age up by 1. Use try / except to handle errors. 
     # If our function is given an incorrect datatype, it should handle the TypeError exception and alert the user
     # pet_birthday(10) => "Happy Birthday! Your pet is now 11."
     # pet_birthday("oops") => "Type Error Occurred"
+
+def pet_birthday(age):
+
+    #attempt to execute code
+    try:
+        return f"Happy Birthday! Your pet is now {age+1}"
+
+#if an error of a particular type occurs,
+#execute soem other bit of code
+
+    except TypeError:
+        return "Type Error Occurred"
+
+print(pet_birthday(4))
+
 
     # Note => To view more common Python exceptions, visit https://docs.python.org/3/library/exceptions.html
 
